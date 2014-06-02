acaa5ca19bca33a943a349b4b8931ff7319950e5
Jasper den Ouden <o.jasper@gmail.com>
2014-06-02 04:04:46 +0200

    Added initially needing to register and made all the indicators live.

--- src/js.js
+++ src/js.js
@@ -20,22 +20,23 @@ var own_address = "TODO";
 // NOTE: really inconvenient without good state-of-blockchain tracking.
 var participated = {};
 
-var from_time = 1000; //eth.getStorageAt(vote_address, own_address);
+var var_from_time = 0; //eth.getStorageAt(vote_address, own_address);
+function from_time()
+{   return var_from_time; }
 
 function power_available()  // Amount of time available to spend.
-{   return date.getTime() - from_time;
-}
+{   return date.getTime() - from_time(); }
 
-function show_power_time()
+function update_power_time()
 {   
     date = new Date();
     power_time.innerHTML = power_available();
 }
 
 var old_spend_val = 0;
-function spend_change()
+function update_spend_time()
 {   
-    show_power_time();
+    update_power_time();
     
     if( spend_time.value < 0 )  // Reset stuff that is disallowed.
     { amount_note.innerHTML = 'Negative disallowed';
@@ -49,14 +50,10 @@ function spend_change()
       spend_time.value = old_spend_val;
       return;
     }
-    amount_note.style = 'color:#777'
     amount_note.innerHTML = '(ok)';
+    amount_note.style = 'color:#777'
     old_spend_val = spend_time.value;
 }
-
-spend_time.value= old_spend_val;
-show_power_time();
-
 
 /*
 // Will be called when transaction passes. (when entered into some block?)
@@ -72,29 +69,54 @@ function complete_spend(vote_for)
     }
 }*/
 
+function pretend_transact(vote_for, amount)
+{
+//    alert(amount);
+//    var_from_time = var_from_time + amount;
+}
+
 function do_spend_time(vote_for, amount)
 {   
     if(amount < power_available())  // Have enough.
     {//eth.transact(_sec, 0, vote_address, bin(vote_for), 1000, 1, complete_spend(vote_for));
+        pretend_transact(vote_for, amount);
         participated[vote_for] = {'amount':amount, 'passed':false};
         update_progress();
     }
 }
 
-function spend_time_button()
+function update_spend_addr()
 {
-    // TODO check:
-    // * validity of address? (though suppose Ethereum might implement it)
-    // * against accidental repeat?
-    // * against spending limit of topic?
+    update_power_time();
+    if( spend_addr.value == '' )
+    { spend_addr_note.innerHTML='Needs value';
+      spend_addr_note.style='color:#444';
+      return;
+    }
     if( participated[spend_addr.value] == null )
-    { do_spend_time(spend_addr.value, spend_time.value); 
-      spend_addr_note.innerHTML='';
+    { spend_addr_note.innerHTML='(ok)';
+      spend_addr_note.style = 'color:#777'
     }
     else
     { spend_addr_note.innerHTML='Already voted for';
       spend_addr_note.style='color:red';
     }
+}
+
+function spend_time_button()
+{
+    if( spend_addr.value == '' ){ return; }  // Cant vote on empty.
+    update_spend_time();
+    update_spend_addr();
+
+    // TODO check:
+    // * validity of address? (though suppose Ethereum might implement it)
+    // * against accidental repeat?
+    // * against spending limit of topic?
+    if( participated[spend_addr.value] == null )
+    { do_spend_time(spend_addr.value, spend_time.value);  }
+    spend_addr.value = ''
+    update_spend_addr()
 }
 
 function update_progress()
@@ -114,3 +136,20 @@ function update_progress()
     if( progress_innerHTML != '' )
     {   progress.innerHTML = '<h4>Transactions in progress</h4><table>' + progress_innerHTML + '</table>'; }
 }
+
+function voting(which)
+{
+    document.getElementById("voting_whole").hidden = which;
+    document.getElementById("register_button").hidden = !which;
+}
+
+function register()
+{   var_from_time = date.getTime();
+    voting(false);
+    update_power_time();
+}
+
+voting(from_time() == 0);
+spend_time.value= old_spend_val;
+update_power_time();
+update_spend_addr();
